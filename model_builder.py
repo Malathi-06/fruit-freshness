0001c60b13585e3865dd02a6946681951e590d14
Malathi-06 <kit27.am28@gmail.com>
2025-12-02 19:51:55 +0530

    Add files via upload

---
+++ model_builder.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def build_model(num_classes):
+    base = tf.keras.applications.MobileNetV2(
+        input_shape=(128, 128, 3),
+        include_top=False,
+        weights='imagenet'
+    )
+    base.trainable = False
+    model = models.Sequential([
+        base,
+        layers.GlobalAveragePooling2D(),
+        layers.Dropout(0.3),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
